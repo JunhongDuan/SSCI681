77c8175306268a9a8f8fa2823093b8eb93273a6a
angle-zhang <angelazhang1220@gmail.com>
2024-10-29 15:03:40 -0700

    descriptive stats and figures for crime data

--- Data_collection.R
+++ Data_collection.R
@@ -1,8 +1,38 @@
 library(crimedata)
-libary(tidyverse)
+library(tidyverse)
 library(dplyr)
 library(lubridate)
+library(tigris)
+library(osmdata)
+library(osmextract)
+library(tidycensus)
 
+
+library(sf)
+library(crsuggest)
+
+state <- "CA"
+county <- "Los Angeles"
+
+# ------ GET MAP FEATURES ------ #
+
+# osm parks 
+osm_parks <- getbb("Los Angeles", display_name_contains = "United States") %>%
+  opq() %>% 
+  add_osm_feature(key = 'leisure', value="park") %>% 
+  osmdata_sf() 
+
+la_parks <- osm_parks$osm_polygons %>%
+  st_transform(proj_crs) %>%
+  st_union() # treat parks as one object
+
+# tigris water
+la_water <- area_water(state="CA", county="Los Angeles", year=2019) %>%
+  st_transform(proj_crs)
+
+# ------ GET CRIME DATA ------ #
+
+# get crime data
 crime_data <- get_crime_data(
   years = 2010:2019, 
   cities = c("Los Angeles"), 
@@ -10,6 +40,46 @@ crime_data <- get_crime_data(
   output="sf"
 )
 
+# get crs
+proj_crs <- as.numeric(suggest_crs(crime_data)$crs_code[1])
+
 la_crime <- crime_data %>%
   tidyr::separate(date_single, c("date", "time"), sep = " ", remove = TRUE) %>%
-  mutate(year = year(date))
+  st_transform(crs=proj_crs) %>%
+  mutate(year = year(date)) 
+
+
+# aggregate crime data to census block then tract level 
+la_crime_ag <- la_crime %>%
+  group_by(year, offense_against, offense_group, offense_type, census_block) %>%
+  summarise(n = n()) 
+
+
+# la geographic data for one year, TODO: make table of all years
+la_pop <- get_acs(
+  geography = "tract",
+  variables = "B01003_001", # Total population
+  state = state,
+  county = county,
+  year = 2019, 
+  survey = "acs5" 
+) 
+
+la_tracts <- tracts(county = "Los Angeles", year = 2019, state = "CA") %>%
+  merge(la_pop, by="GEOID") %>%
+  st_transform(proj_crs)
+
+crime_tract <- la_tracts %>%
+  st_join(la_crime, join = st_contains) 
+
+crime_tract_ag <- crime_tract %>%
+  st_drop_geometry() %>%
+  #group_by(year, offense_against, offense_group, offense_type, GEOID) %>%
+  group_by(year, GEOID) %>%
+  summarise(n = n()) %>%
+  merge(la_tracts, by="GEOID") %>%
+  filter(estimate > 10) %>% # population per tract must be greater than 10
+  mutate(crime_rate= 1000*n/estimate) %>% # crime rate
+  st_as_sf()
+
+# ------ HEAT DATA TABLES ------ #
--- Descriptive_stats.R
+++ Descriptive_stats.R
@@ -3,7 +3,11 @@ library(ggmap)
 library(tigris)
 library(janitor)
 library(sf)
+library(spdep)
+
 library(kableExtra)
+
+library(tmap)
 ### HEAT DATA TABLES 
 
 # total crime count
@@ -21,4 +25,35 @@ crime_count %>% tabyl(offense_group, year) %>%
   knitr::kable() %>%
   kable_styling("basic")
   
+crime_tract_2019 <- crime_tract_ag %>% filter(year==2019)
+
+# map count
+tm_shape(crime_tract_2019) + 
+  tm_polygons(col="n", size=.1, style="fisher") +
+  # tm_shape(la_water) +
+  # tm_polygons(col="lightblue", border.alpha = 0, alpha=.5) +
+  # tm_shape(la_parks) +
+  # tm_polygons(col="#6E9A81", border.alpha = 0, alpha=.5) +
+  tm_compass(size=1) +
+  tm_scale_bar(position = c("left", "bottom"))
+
+# map crime rate per 1000
+tm_shape(crime_tract_2019) + 
+  tm_polygons(col="crime_rate", size=.1, style="fisher") +
+  # tm_shape(la_water) +
+  # tm_polygons(col="lightblue", border.alpha = 0, alpha=.5) +
+  # tm_shape(la_parks) +
+  # tm_polygons(col="#6E9A81", border.alpha = 0, alpha=.5) +
+  tm_compass(size=1) +
+  tm_scale_bar(position = c("left", "bottom"))
+
 # moran's I 
+coords <- st_coordinates(crime_tract_2019) # Create a spatial weights list using Queen contiguity
+nb <- poly2nb(crime_tract_2019)  # Create neighbors list
+lw <- nb2listw(nb, style = "W")  # Convert to a spatial weights list
+
+moran_test_crime_rate <- moran.test(crime_tract_2019$crime_rate, lw)
+print(moran_test_crime_rate)
+
+moran_test_crime_ct <- moran.test(crime_tract_2019$n, lw)
+print(moran_test_crime_ct)
